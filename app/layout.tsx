f798843704cec4b0de3f6815979363d2f3420f1a
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-27 10:04:37 -0500

    EDIT: wrap app with providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { geistMono, geistSans } from "./fonts";
+import { Providers } from "./providers";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Phylax Transaction Decoder",
   description: "Decoding ETH transactions and traces via a nested table visualization",
 };
 
-export default function RootLayout({
+export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -27,11 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
-        <NavBar />
-        <main className="flex-1 pb-10">
-          {children}
-        </main>
-        <Footer />
+        <Providers>
+          <NavBar />
+          <main className="flex-1 pb-10">
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
