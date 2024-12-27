4c492e8da342575885a6957f768b583738af3a69
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-27 09:36:19 -0500

    NEW: footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="sticky bottom-0 border-t dark:border-gray-800 bg-white dark:bg-gray-900">
+      <div className="mx-auto px-6 py-2 mb-[2px]">
+        <div className="flex justify-end items-center">
+          <div className="ml-auto">
+            <span className="text-gray-600 dark:text-gray-400">
+              Developed by{' '}
+              <a
+                href="https://github.com/kamwithak"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 dark:text-blue-400 hover:text-blue-700 dark:hover:text-blue-300"
+              >
+                Kamran Choudhry
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+} 
