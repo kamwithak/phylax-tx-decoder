7fd32019bcd70c18b6fe072f368f6ca4e5ad9561
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-27 09:36:27 -0500

    EDIT: use the footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,14 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="h-full">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
         <NavBar />
-        <main>
+        <main className="flex-1 pb-10">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
