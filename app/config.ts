44c1e97fea6e3ed81a687e5b286313f6c6702bd9
Kamran Choudhry <intractable.truth@gmail.com>
2024-12-27 10:04:17 -0500

    EDIT: config file

---
+++ app/config.ts
@@ -0,0 +1,16 @@
+import { http, createConfig } from 'wagmi'
+import { arbitrum, mainnet, sepolia } from 'wagmi/chains'
+import { injected, walletConnect } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [mainnet, sepolia, arbitrum],
+  connectors: [
+    injected(),
+    walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? '' }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [arbitrum.id]: http(),
+  },
+})
